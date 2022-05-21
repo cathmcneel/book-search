3c0e7c3071fa77b98641ccbaa721d86760b84500
Catherine McNeel <catherinemcneel@icloud.com>
2022-05-21 18:44:26 -0400

    added queries

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -2,7 +2,27 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+      _id:  ID
+      username: String
+      email: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+      me: User
+      users: [User]
+      user(username: String!): User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook: String!
+  }
+`;
 
 // export the typeDefs
 module.exports = typeDefs;
